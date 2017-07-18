5a451814a8baee8ce5a2e2102e3fec3d02beaf9b
hugo forte <hforte@gcmlp.com>
2017-07-18 15:22:08 -0500

    no message

---
+++ Ftp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using Microsoft.Office.Interop.Outlook;
+
+namespace OutlookExporter
+{
+    public static class FtpHelper
+    {
+        public static string UserName = "hugoforte";
+        public static string PassWord = "ruq0ljxn";
+        public static string FtpSite = "ftp://ftp.axelforte.com/";
+        public static void Upload(string fromPath, string toPath)
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.Credentials = new NetworkCredential(UserName, PassWord);
+                client.UploadFile($"{FtpSite}/{toPath}", "STOR", fromPath);
+            }
+        }
+
+        public static void Download(string fromPath, string toPath)
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.Credentials = new NetworkCredential(UserName, PassWord);
+                client.DownloadFile($"{FtpSite}/{fromPath}", toPath);
+            }
+        }
+    }
+
+}
---
+++ OutlookExporter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Office.Interop.Outlook;
+
+namespace OutlookExporter
+{
+    public static class OutlookExporter
+    {
+        public static void ExportIcal(string path)
+        {
+            var oApp = new Application();
+            var mapiNamespace = oApp.GetNamespace("MAPI"); ;
+
+            MAPIFolder f = mapiNamespace.GetDefaultFolder(OlDefaultFolders.olFolderCalendar);
+
+            CalendarSharing cs = f.GetCalendarExporter();
+            cs.CalendarDetail = OlCalendarDetail.olFullDetails;
+            cs.StartDate = DateTime.Now.AddMonths(-1);
+            cs.EndDate = DateTime.Now;
+            cs.SaveAsICal(path);
+        }
+    }
+
+}
